fe82d1d0e2eec9846d5b3019c295821bff7ec34b
Camila <camila.matosrocha1234@gmail.com>
2022-08-15 14:26:03 -0300

    Chamada HTTP feita

--- src/app/courses/services/courses.service.ts
+++ src/app/courses/services/courses.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 import { Course } from '../model/course';
-import { first, tap } from 'rxjs/operators';
+import { delay, first, tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -18,7 +18,9 @@ export class CoursesService {
   list()  {
     return this.httpClient.get<Course[]>(this.API)
     .pipe(
-      //take(1): assim que o servidor me der uma resposta, eu vou pegar essa resposta e finalizar a conexão ocm a origem dos dados
+      //take(1): assim que o servidor me der uma resposta, eu vou pegar essa resposta e finalizar a inscrição com a origem dos dados
+      // first: só pega a primeira resposta que o servidor enviar
+      delay(1000),
       first(),
       tap(courses => console.log(courses))
     );
