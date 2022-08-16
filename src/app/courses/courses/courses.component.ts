3b333a855f3b97e8ff07ad163eac4e0580c6c3c1
Camila <camila.matosrocha1234@gmail.com>
2022-08-16 16:38:48 -0300

    Componente de formulário e roteamento para criar cursos

--- src/app/courses/courses/courses.component.ts
+++ src/app/courses/courses/courses.component.ts
@@ -5,6 +5,7 @@ import { catchError, Observable, of } from 'rxjs';
 import { Course } from '../model/course';
 import { CoursesService } from '../services/courses.service';
 import { ErrorDialogComponent } from '../../shared/components/error-dialog/error-dialog.component';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-courses',
@@ -14,13 +15,16 @@ import { ErrorDialogComponent } from '../../shared/components/error-dialog/error
 export class CoursesComponent implements OnInit {
 
   courses$: Observable<Course[]>;
-  displayedColumns = ['name', 'category'];
+  displayedColumns = ['name', 'category', 'actions'];
 
   // coursesService: CoursesService;
 
   constructor(
     private coursesService: CoursesService,
-    public dialog: MatDialog ) {
+    public dialog: MatDialog,
+    private router: Router,
+    private route: ActivatedRoute,
+    ){
     // this.courses = [];
     // this.coursesService = new CoursesService();
     this.courses$ = this.coursesService.list()
@@ -40,6 +44,10 @@ export class CoursesComponent implements OnInit {
 
   ngOnInit(): void {
 
+  }
+
+  onAdd() {
+    this.router.navigate(['new'], {relativeTo: this.route});
   }
 
 }
